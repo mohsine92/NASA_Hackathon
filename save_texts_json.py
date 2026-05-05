be37d9b1a39fda9df6365d2ec0d3800f751bc8c3
mohsine92 <mohsine.essat@gmail.com>
2026-05-05 14:12:49 +0200

    fix: remove unused scripts and __pycache__

--- save_texts_json.py
+++
@@ -1,7 +0,0 @@
-import json
-from generate_text import generate_exoplanet_texts
-
-texts = generate_exoplanet_texts()
-
-with open("data/exoplanet_texts.json", "w") as f:
-    json.dump(texts, f, indent=2)
